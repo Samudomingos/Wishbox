f1afbdf7dbca4b3466c8429a2a4008e6b66dd278
Samuel <samudomingos2018@gmail.com>
2019-04-09 15:52:29 -0300

    Pagina Home criada

---
+++ index.php
@@ -0,0 +1,270 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content=""> 
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+    <title>Gráfica WishBox</title>
+  </head>
+  <body class="bg-dark">
+      <header>
+        <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-warning rounded">
+          <a class="navbar-brand" href="#">
+            <img src="assets/images/Logo.png" class="img-fluid" alt="Responsive image" width="150px">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item mr-3">
+                <a class="nav-link text-dark" href="index.php">Home</a>
+              </li>
+              <li class="nav-item mr-3">
+                <a class="nav-link text-dark" href="#">Clientes</a>
+              </li>
+              <li class="nav-item mr-3">
+                <a class="nav-link text-dark" href="#">Serviços</a>
+              </li>
+              <li class="nav-item mr-3">
+                <a class="nav-link text-dark" href="#">Fale Conosco</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Redes Sociais</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item text-success" href="#">WhatsApp <i class="fab fa-whatsapp-square"></i></a>
+                  <a class="dropdown-item text-primary" href="#">Facebook <i class="fab fa-facebook"></i></a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+<div id="carouselExampleIndicators" class="carousel slide mt-5" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="assets/images/bg.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/images/bg2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/images/bg-3.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>        
+        <div class="jumbotron mt-5 pb-2 mb-0 bg-light">
+          <h1 class="display-4">WishBox!</h1>
+          <p class="lead text-dark">Estamos sempre prontos para novos desafios, nos deixe transformar os seus desejos em embalagens, folders, cartões de visita enfim no que há de melhor no ramo gráfico!</p>
+          <hr class="my-4">
+          <p class="lead">
+            <a class="btn btn-secondary btn-lg" href="#" role="button">Learn more</a>
+          </p>
+        </div>
+        <p>!</p>
+        <div class="row ml-3 pt-3">
+          <div class="col-sm">
+            <div class="card text-white bg-dark mb-3 border border-info" style="max-width: 18rem;">
+              <div class="card-header d-flex">Para seu Negócio <i class="fas fa-user-tie ml-auto mt-1"></i></div>
+              <div class="card-body">
+                <h5 class="card-title">Divulgação de Qualidade</h5>
+                <p class="card-text">Cartão de visitas, folders, agendas e o que mais você precisar!</p>
+                <a href="" class="btn btn-primary">Saiba mais</a>
+              </div>
+            </div>            
+          </div>  
+          <div class="col-sm">
+            <div class="card text-white bg-dark mb-3 border border-info" style="max-width: 18rem;">
+              <div class="card-header d-flex">Para sua Casa <i class="fas fa-home ml-auto mt-1"></i></div>
+              <div class="card-body">
+                <h5 class="card-title">Mais bonita</h5>
+                <p class="card-text">Adesivos personalizados, Convites e imãs de geladeira!</p>
+                <a href="" class="btn btn-primary">Saiba mais</a>
+              </div>
+            </div>            
+          </div>
+          <div class="col-sm">
+            <div class="card text-white bg-dark mb-3 border border-info" style="max-width: 18rem;">
+              <div class="card-header d-flex">Para Você <i class="fas fa-heart ml-auto mt-1"></i></div>
+              <div class="card-body ">
+                <h5 class="card-title">Bem Representado</h5>
+                <p class="card-text">Laminação fosca e brilho, Alta resolução na impressão!</p>
+                <a href="" class="btn btn-primary">Saiba mais</a>
+              </div>
+            </div>            
+          </div>                    
+        </div>
+
+      <div class="card mt-5 mb-3 border border-dark">
+        <h5 class="card-header bg-warning">Destaque</h5>
+        <div class="card-body">
+          <h5 class="card-title">Um tratamento mais que especial</h5>
+          <p class="card-text">A Wishbox acredita que uma imagem vale mais que mil palavras, por isso ela prima pela qualidade de seus produtos e serviços para garantir a seus clientes total satisfação, oferecendo-lhes o que há de melhor em soluções gráficas!
+          Deixe-nos fazer parte da sua história imprimindo os seus sonhos e desejos!</p>
+          <a href="#" class="btn btn-primary">Saiba Mais</a>
+        </div>
+      </div>
+      <hr class="mx-3 my-5 bg-secondary" >
+<!-- Footer -->
+<footer class="page-footer font-small stylish-color-dark pt-4">
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+
+      <!-- Grid row -->
+      <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-md-4 mx-auto">
+
+          <!-- Content -->
+          <h5 class="font-weight-bold text-uppercase mt-3 mb-4 text-light">Footer Content</h5>
+          <p class="text-light">Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit.</p>
+
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none">
+
+        <!-- Grid column -->
+        <div class="col-md-2 mx-auto">
+
+          <!-- Links -->
+          
+
+        </div>
+        <!-- Grid column -->
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+          <!-- Links -->
+          <h5 class="text-uppercase font-weight-bold text-light mb-4">Contact</h6>
+          <p class="text-light">
+            <i class="fas fa-home mr-3 text-light"></i> New York, NY 10012, US</p>
+          <p class="text-light">
+            <i class="fas fa-envelope mr-3 text-light"></i> info@example.com</p>
+          <p class="text-light">
+            <i class="fas fa-phone mr-3 text-light"></i> + 01 234 567 88</p>
+          <p class="text-light">
+            <i class="fas fa-print mr-3 text-light"></i> + 01 234 567 89</p>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <hr>
+
+    <!-- Call to action -->
+    <ul class="list-unstyled list-inline text-center py-2">
+      <li class="list-inline-item">
+        <h5 class="mb-1 text-light">Receba nossas promoções</h5>
+      </li>
+      <li class="list-inline-item">
+        <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Cadastre-se
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header  bg-warning">
+        <h5 class="modal-title" id="exampleModalLabel">Receba nossas promoções</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form method="GET">
+        <div class="form-group">        
+        <div class="modal-body bg-info">
+          <h5> Fique por dentro de nossas promoções!</h5>
+           <label for="exampleInputEmail1">Endereço de Email</label>
+           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="seuemail@email.com">
+        </div>
+        <div class="modal-footer bg-info">    
+          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+        </div>
+      </form>  
+    </div>
+  </div>
+</div>
+      </li>
+    </ul>
+    <!-- Call to action -->
+
+    <hr>
+
+    <!-- Social buttons -->
+    <ul class="list-unstyled list-inline text-center">
+      <li class="list-inline-item">
+        <a class="btn-floating btn-fb mx-1">
+          <i class="fab fa-facebook-f text-light"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-tw mx-1">
+          <i class="fab fa-twitter text-light"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-gplus mx-1">
+          <i class="fab fa-google-plus-g text-light"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-li mx-1">
+          <i class="fab fa-linkedin-in text-light"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-dribbble mx-1">
+          <i class="fab fa-dribbble text-light"> </i>
+        </a>
+      </li>
+    </ul>
+    <!-- Social buttons -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3 text-light">© 2018 Copyright:
+      <a href="https://mdbootstrap.com/education/bootstrap/">samudomingos.com</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
+
+        </div>
+      </head>    
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
